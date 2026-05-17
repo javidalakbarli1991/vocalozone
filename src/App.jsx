e217ce178073f315048c7c0b2ac3f40318110820
javidalakbarli1991 <javidalakbarli1991@gmail..com>
2026-05-17 18:58:06 +0400

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -9,7 +9,7 @@ import WhatIs from './pages/WhatIs'
 
 function App() {
   return (
-    <BrowserRouter basename="/vocalozone/">
+    <HashRouter>
       <div className="relative min-h-screen bg-[#f8fafc] text-slate-900">
         <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_20%_10%,rgba(251,146,60,0.25),transparent_35%),radial-gradient(circle_at_80%_0%,rgba(34,211,238,0.22),transparent_32%),linear-gradient(180deg,#fff_0%,#f8fafc_50%,#fff_100%)]" />
         <div className="relative">
@@ -27,7 +27,7 @@ function App() {
           <Footer />
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
